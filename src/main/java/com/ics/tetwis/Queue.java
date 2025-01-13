795c53f370eb70c1be17029a2b54869226df98f1
Qi646 <qining646@gmail.com>
2025-01-13 14:08:01 -0500

    Queue

---
+++ src/main/java/com/ics/tetwis/Queue.java
@@ -0,0 +1,46 @@
+public class Queue {
+    private int[] queue;
+    private int front, rear, size;
+
+    public Queue(int initialCapacity) {
+        queue = new int[initialCapacity];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    public void enqueue(int value) {
+        if (size == queue.length) {
+            resize();
+        }
+        rear = (rear + 1) % queue.length;
+        queue[rear] = value;
+        size++;
+    }
+
+    public int dequeue() {
+        if (size == 0) {
+            return -1;
+        }
+        int value = queue[front];
+        front = (front + 1) % queue.length;
+        size--;
+        return value;
+    }
+
+    public int peek() {
+        if (size == 0) {
+            return -1;
+        }
+        return queue[front];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
+
