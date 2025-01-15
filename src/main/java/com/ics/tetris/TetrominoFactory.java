c32ffd92f2d897142befa23704b7aae3f983c258
Benjamin Lu <templu1107@proton.me>
2025-01-14 20:36:58 -0500

    Refactor TetrominoFactory to use a shuffled bag for random Tetromino generation

--- src/main/java/com/ics/tetris/TetrominoFactory.java
+++ src/main/java/com/ics/tetris/TetrominoFactory.java
@@ -1,11 +1,33 @@
 package com.ics.tetris;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class TetrominoFactory {
-  private final Random random = new Random();
+
+  private List<Integer> bag;
+  private int index;
+
+  public TetrominoFactory() {
+    bag = new ArrayList<>();
+    index = 0;
+    refillBag();
+  }
+
+  private void refillBag() {
+    bag.clear();
+    for (int i = 0; i < 7; i++) {
+      bag.add(i);
+    }
+    Collections.shuffle(bag);
+  }
 
   public Tetromino createTetromino() {
-    return new Tetromino(random.nextInt(7));
+    if (index >= bag.size()) {
+      refillBag();
+      index = 0;
+    }
+    return new Tetromino(bag.get(index++));
   }
 }
